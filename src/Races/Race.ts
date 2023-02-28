d4ff1e074c09e31690ca585f01a6998766e27561
VitorBarrioni <vitor_teixeira90@hotmail.com>
2023-02-28 11:36:26 -0300

    req 3 e 4

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,6 +1,6 @@
 export default abstract class Race {
-  private _name: string;
-  private _dexterity: number;
+  private readonly _name: string;
+  private readonly _dexterity: number;
   constructor(name: string, dexterity: number) {
     this._name = name;
     this._dexterity = dexterity;
@@ -8,6 +8,14 @@ export default abstract class Race {
 
   static createdRacesInstances() : number {
     throw new Error('Not implemented');
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
   }
 
   abstract get maxLifePoints(): number;
